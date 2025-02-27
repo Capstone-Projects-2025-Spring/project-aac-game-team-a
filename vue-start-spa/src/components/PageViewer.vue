2ffada54ac512a5d168f9ef6e4919178fa284737
Kenneth <tuo64805@temple.edu>
2025-02-27 15:31:30 -0500

    Using Route Params

--- vue-start-spa/src/components/PageViewer.vue
+++ vue-start-spa/src/components/PageViewer.vue
@@ -2,22 +2,15 @@
     <!-- This is our template to be used by Vue.creatApp() to generate content-->
     <div class="container">
         <h1 class="emphasize">Page Title</h1>
-        <p>Content</p>
+        <p>{{$route.params.index}}</p>
     </div>  
 </template>
 
 <script>
 export default {
-    props: {
-        page : {
-            type: Object,
-            default(rawProps){
-                return{
-                    pageTitle: '',
-                    content: ''
-                };
-            }
-        }
+    created() {
+        //console.log(this.$route.params);
+
     }
 }
 </script>
