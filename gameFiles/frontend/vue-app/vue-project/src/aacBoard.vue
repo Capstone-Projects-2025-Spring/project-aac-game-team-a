3b9c571ab249d21a3444afe7db82ea25078ccb21
Isaac Schwartz <tuo62395@temple.edu>
2025-02-27 17:54:09 -0500

    added asset files for aac board and made them appear on each item

--- gameFiles/frontend/vue-app/vue-project/src/aacBoard.vue
+++ gameFiles/frontend/vue-app/vue-project/src/aacBoard.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="aac-board">
-        <!-- This button only appears if currentCategory is not null. -->
         <button v-if="currentCategory" @click="currentCategory = null">Back</button> 
 
+        <!-- Grid for categories. -->
         <div v-if="!currentCategory" class="grid">
 
             <button v-for="(items, category) in categories" :key="category" @click="currentCategory = category">
-                {{category}}
+                <!-- image display for category -->
+                <img :src="getCategoryImage(category)" :alt="category" class="category-image"/>
+                <p>{{category}}</p>
             </button>
         </div>
 
+        <!-- Grid for items in a category. -->
         <div v-else class="grid">
             <button v-for="item in categories[currentCategory]" :key="item">
-                {{item}}
+                <img :src="getItemImage(currentCategory, item)" :alt="item" class="item-image"/>
+                <p>{{item}}</p>
             </button>
         </div>
     </div>
@@ -28,7 +32,20 @@ const categories = {
     Food: ['Apple', 'Pizza', 'Carrot', 'Spaghetti', 'Banana', 'Grapes'],
     Clothing: ['Shirt', 'Pants', 'Hat', 'Glasses', 'Shoe', 'Glove'],
     Actions: ['Run', 'Jump', 'Eat', 'Sleep'],
-    Shapes: ['Circle', 'Square', 'Triangle']
+    Shapes: ['Circle', 'Square', 'Triangle', 'Oval']
+};
+
+//List of categories for photos
+const categoryList = Object.keys(categories);
+
+//Function that gets url for category images
+const getCategoryImage = (category) => {
+    return new URL(`./assets/aacSymbols/Categories/${category.toLowerCase()}.png`, import.meta.url).href;
+};
+
+//Function to get item images for each category
+const getItemImage = (category, item) => {
+    return new URL(`./assets/aacSymbols/${category}/${item.toLowerCase()}.png`, import.meta.url).href;
 };
 </script>
 
@@ -56,5 +73,11 @@ button {
     font-size: 70px;
     cursor: pointer;
     border-radius: 10px;
+}
+
+.category-image, .item-image {
+    width: 50px;
+    height: 50px;
+    margin-bottom: 10px;
 }
 </style>
