f2b66af2c1926936f89f8b4a0d98c690aab640a3
Lincoln Mcloud <ljtron10@gmail.com>
2025-02-28 10:20:49 -0500

    setting websocket to socket.io

---
+++ gameFiles/backend/starter.cjs
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  var address = socket.handshake.address;
+  console.log('New connection from ' + address.address + ':' + address.port);
+  console.log('a user connected');
+
+  var client_ip_address = socket.request.connection.remoteAddress;
+  console.log('New connection from ' + client_ip_address);
+
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+// import { WebSocketServer } from 'ws';
+
+// const wss = new WebSocketServer({ port: 8080 });
+// const games = []
+
+// wss.on('connection', function connection(ws, req) {
+//   // const ip = req.socket.remoteAddress;
+//   console.dir(ws)
+//   console.log("ip address of user: " + ip)
+//   ws.on('error', console.error);
+
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
+
+//   // starts a game session
+//   ws.on('startSession', function gameRoom(data) {
+//     ws.emit(data)
+//   })
+
+//   ws.send('something');
+// });
+
+// // a quick response to know the websocket started
+// console.log("websocket started")
