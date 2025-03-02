4eec6863824f5ef2466340df33bba4bb26188893
Terrell Heyward <67223055+tlheyward@users.noreply.github.com>
2025-03-02 01:25:04 +0000

    Excluded unnecessary files from unit and coverage testing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['text', 'json', 'html']
-    },
+      exclude: [
+        '**/documentation/**',
+        '**/node_modules/**',
+        '**/src/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier,vue}.config.*'
+      ],
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './documentation/static/coverage'
+    }
   },
 })
