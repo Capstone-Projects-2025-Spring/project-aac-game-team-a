af3e21dd02882e95ef90fc20d1d92bb182b461c8
Kenneth <tuo64805@temple.edu>
2025-03-10 10:11:15 -0400

    Updated port where local host connects to on client side

--- scribblersGame/src/components/ScribblersApp.vue
+++ scribblersGame/src/components/ScribblersApp.vue
@@ -96,7 +96,7 @@ export default {
 
             // CHANGE THIS WHEN YOU WANT THE SERVER TO BE PUBLIC
 
-            this.socketInstance = io("http://localhost:3000");
+            this.socketInstance = io("http://localhost:3001");
             // this.socketInstance = io("http://[YOUR IP HERE]:3000");
 
             // Listen for incoming messages from the server and update messages array
