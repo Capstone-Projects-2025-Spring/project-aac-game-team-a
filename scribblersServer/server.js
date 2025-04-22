cbf8515ed090dd1030a232143cf02be79bdcc817
Terrell Heyward <67223055+tlheyward@users.noreply.github.com>
2025-04-21 23:31:47 -0400

    Moved server initialization, server start, and listener initialization functions into SockerServerHandler

--- scribblersServer/server.js
+++ scribblersServer/server.js
@@ -1,34 +1,22 @@
 /**
- * 
+ * This file it the main executable for the Scribblers backend. Node, Express, and Socket.io 
+ * are used to listen for requests and send/recieve game data.
  */
-const exp = require('constants'); // Importing constants module (not needed in this case)
-const express = require('express'); // Importing Express.js to create the server
-const http = require('http'); // Importing HTTP module to create an HTTP server
-const { Server } = require('socket.io'); // Importing the Server class from socket.io
+//  IMPORTING
+const express = require('express'); // Creates backend server
+const http = require('http'); // Creates HTTP server
+const SocketServerHandler = require("./objects/SocketServerHandler.js"); // Manages socket server interactions
 
-const app = express(); // Initializing an Express application
-const server = http.createServer(app); // Creating an HTTP server using Express
+//  INITIALIZATIONS
+const app = express(); // Express initialization
+const server = http.createServer(app); // Create HTTP server using Express
+const mappedGameData = new Map(); // Create global map to manage game data objects
+const SocketServer = new SocketServerHandler(); // Create socket manager
+const io = SocketServer.createServerInstance(server); // Create Socket.io server instance
 
-const GameData = require("./objects/GameData") // Holds all game data needed by server
-const SocketHandlerClass = require("./objects/SocketHandler")
-const GameSessionsDBClass = require("./Database/GameSessions.js");
-
-// Initializing a new Socket.io server instance and configuring CORS
-const io = new Server(server, {
-    cors: {
-        origin: "*", // Allows connections from any origin (use restrictive policies in production)
-        methods: ['GET', "POST"] // Specifies allowed HTTP methods
-    }
-});
-
+//  GAME SETTINGS
 const roundTimerLength = 60; //set length of round timer
-let playerCount = 0; //number of player that have joined
-let playersQueue = []; //queue of players by socket ID
 let imagesPerPrompt = 3; // represents the amount of images to choose from per prompt
-let messageBoard = []; // Used to display messages next to avatars in game
-
-const mappedGameData = new Map();
-const GameSessionDB = new GameSessionsDBClass()
 
 // Drawing prompt objects categorized by type
 const promptList = [
@@ -102,157 +90,16 @@ function clearGuesses(room) {
 
 // Event listener for new socket connections
 io.on('connection', (socket) => {
-
-    //  Listen for request to create new lobby
-    socket.on("create-new-lobby", (numRounds, maxPlayers, players, user) => {
-        
-        //  Generate new lobby code
-        let randomCodeDigits = [];
-        let uniqueCodeFound = false;
-
-        while (!uniqueCodeFound) {
-            
-            //  Generate random values in array
-            randomCodeDigits.value = Array.from({ length: 4 }, () =>
-                Math.floor(Math.random() * 9) + 1
-            );
-
-            //  Check if value is in mapped array
-            const newCodeString = randomCodeDigits.value.join('');
-            if (io.sockets.adapter.rooms.has(newCodeString)) {
-                //  Do nothing and cycle through while loop to try again
-            }
-            else{
-                uniqueCodeFound = true;
-                io.to(socket.id).emit("update-lobby-code", newCodeString);
-
-                //  Add host to player list if playing
-                let playerData = new Map();
-                if (user) {
-                    playerData.set(user, {currentGuess: "", score: 0})
-                }
-                const gameData = new GameData(numRounds, 0, maxPlayers, players, null, "", 0, 0, playerData);
-                mappedGameData.set(newCodeString, gameData);
-                console.log(mappedGameData);
-            }
-        }
-    });
-
+    
+    SocketServer.initializeServerListeners(io, socket, mappedGameData);
+    
     socket.on('start-game', (code)  => {
         
         console.log(`starting game in room ${code}`)
         socket.to(code).emit('start-game');
         startNewRound(code);
     })
-
-    // Listens for user joining room
-    socket.on('join-room', (code, user, isHost)  => {
-
-        //  Prevent users from joining non-existent rooms, joining full rooms, or duplicate avatars
-        if (io.sockets.adapter.rooms.has(code) && mappedGameData.get(code).playerData.size < mappedGameData.get(code).maxPlayers && !mappedGameData.get(code).playerData.has(user)) {
-            //console.log(`room ${code} exists!`)
-            socket.join(code);
-            mappedGameData.get(code).players.push(user)
-            mappedGameData.get(code).playerData.set(user, {currentGuess: "", score: 0})
-            mappedGameData.get(code).playerData.forEach((value, key) => {
-                io.to(socket.id).emit("add-player", key, value);
-            })
-            io.to(code).emit("update-player-list", mappedGameData.get(code).players);
-            socket.to(code).emit("add-player", user, {currentGuess: "", score: 0});
-            io.to(socket.id).emit("update-max-players", mappedGameData.get(code).maxPlayers);
-            io.to(socket.id).emit("update-round", mappedGameData.get(code).currentRound);
-            io.to(socket.id).emit("update-num-rounds", mappedGameData.get(code).numberRounds);
-            console.log(mappedGameData);
-        }
-        else{
-            //console.log(`room ${code} does not exist!`)
-            socket.join(code);
-            if (!isHost)
-                io.to(socket.id).emit("update-user", '');
-        }
-        console.log(`User ${socket.id} is connected to room ${code}`); // Logs when a new user connects
-    })
-    playerCount++;
-
-    //  Listens for users leaving the room
-    socket.on('leave-room', (code, user) => {
-
-        console.log(`User ${socket.id} with avatar ${user} has disconnected from room ${code}`);
-        socket.leave(code);
-        if (!mappedGameData.has(code) || !mappedGameData.get(code).playerData.has(user)) return;
-
-        //  Loop through array to find and remove player from array
-        let index = 0;
-        let arrLength = mappedGameData.get(code).players.length
-        for (index = 0; index < arrLength; index++) {
-            if (mappedGameData.get(code).players[index] == user) {
-                mappedGameData.get(code).players.splice(index, 1);
-                index = arrLength;
-                //console.log(mappedGameData.get(code).players);
-            }
-        }
-        socket.to(code).emit("update-player-list", mappedGameData.get(code).players);
-        mappedGameData.get(code).playerData.delete(user)
-        socket.to(code).emit('remove-player', user)
-        console.log(mappedGameData.get(code))
-
-        //  Check if room deleted
-        if (!io.of("/").adapter.rooms.get(code)) {
-            clearInterval(mappedGameData.get(code).timerID);
-            console.log(`room ${code} has been deleted`);
-
-            //  Remove game data from deleted room. Delete() function returns false if key does not exist.
-            mappedGameData.delete(code);
-            console.log(mappedGameData);
-        }
-
-        //console.log(io.of("/").adapter.rooms.get(code)); // Print remaining users in room
-    })
-
-    //add player to the queue
-    playersQueue.push(socket.id);
-
-    // Keeps track of players currently in game (needs to be refined to be room specific)
-    io.emit("player-count-update", { playerCount }); // Notify all players of new count
-
-    const SocketHandler = new SocketHandlerClass(io, socket, GameSessionDB)
-    SocketHandler.createGame()
-    SocketHandler.onPlayerJoin()
-    SocketHandler.onRoundStart()
-
-    socket.on("draw_data", (data) => {
-        console.log("draw_data log")
-        try {
-            console.log(data)
-            socket.broadcast.emit('draw_data:received', data)
     
-        } catch (err){
-            console.log("Message data unable to be updated");
-            console.error(err)
-        }
-    });
-    
-    //  Listeners for handling drawing data
-    socket.on("draw-init", (room, x, y, draw_color, draw_width, context) => {
-        socket.to(room).emit("cast-draw-init", x, y, draw_color, draw_width, context);
-    });
-
-    socket.on("draw", (room, x, y) => {
-        socket.to(room).emit("cast-draw", x, y);
-    });
-
-    socket.on("draw-end", (room) => {
-        socket.to(room).emit("cast-draw-end");
-    });
-
-    socket.on("draw-clear", (room) => {
-        socket.to(room).emit("cast-draw-clear");
-    });
-
-    socket.on("draw-undo", (room) => {
-        socket.to(room).emit("cast-draw-undo");
-    });
-
     socket.on("update-user-guess", (room, user, guess, imagePath) => {
         //  update player's guess in game data map and emit to all players in room except sender
         mappedGameData.get(room).playerData.get(user).currentGuess = guess
@@ -333,24 +180,6 @@ io.on('connection', (socket) => {
             console.error(`Error during timer update in room ${room}, clearing timer interval`)
         }
     };
-
-    // Listener for socket disconnections
-    socket.on('disconnect', () => {
-        console.log(`(2)User ${socket.id} disconnected`); // Logs when a user disconnects
-        playersQueue = playersQueue.filter(playerID => playerID !== socket.id); // Remove from queue
-        playerCount = Math.max(0, playerCount - 1); // Decrease player count
-        console.log(`Current player count: ` + playerCount);
-        for(i=0; i<messageBoard.length; i++){
-            if(messageBoard[i].id == socket.id){
-                messageBoard.splice(i);
-                io.emit('update-user-message-board', (messageBoard));
-            }
-        }
-        io.emit("player-count-update", { playerCount }); // Notify others
-    });
 });
 
-// Start the server and listen on port 3000
-server.listen(3001, () => {
-    console.log("Scribblers server is running on port 3001");
-});
+SocketServer.startServer(server, 3001);
